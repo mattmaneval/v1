cefd8f163cb2c81a8b385a2263aba1ba76e87be2
mattmaneval <manevalmusic@gmail.com>
2020-12-16 13:46:58 -0500

    About section base structure

---
+++ wp-content/themes/v1/template-parts/about.php
@@ -0,0 +1,23 @@
+<section class="about">
+  <div class="wrap">
+    <h2>I specialize in building exceptional websites.</h2>
+    <div class="about-card">
+      <div class="content">
+        <h4>About</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Outside of work I enjoy writing and recording music, running, and playing golf.</p>
+        <h4>Technologies I've been using:</h4>
+        <ul>
+          <li>Html</li>
+          <li>Css</li>
+          <li></li>
+          <li>Sass</li>
+          <li>Wordpress</li>
+          <li>React</li>
+          <li>Gatsby</li>
+        </ul>
+      </div>
+      <div class="about-potrait"></div>
+    </div>
+  </div>
+</section>
