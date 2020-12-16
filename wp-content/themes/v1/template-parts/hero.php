23289cb50777b156f1eb93fc5b37cb8c250fd17c
mattmaneval <manevalmusic@gmail.com>
2020-12-16 10:51:23 -0500

    Style hero h1

--- wp-content/themes/v1/template-parts/hero.php
+++ wp-content/themes/v1/template-parts/hero.php
@@ -2,9 +2,9 @@
   <div class="wrap">
     <h1>
       <span>Hi there! My name is</span>
-      <br>
+      <br />
       <span>Matt Maneval and I build exceptional websites.</span>
     </h1>
-    <p>I'm a freelance Web Developer in Toledo, Ohio specializing in building (and tipping my toes in designing) things for the web. I'm really proud to be working under the mentorship of the good folks<a href="https://electricfun.io/" target="_blank" rel="noopener noreferrer" title="Vist the good folks at Electric Fun">@ElectricFun.</a></p>
+    <p>I'm a freelance Web Developer in Toledo, Ohio specializing in building (and tipping my toes in designing) things for the web. I'm really proud to be working under the mentorship of the good folks <a href="https://electricfun.io/" target="_blank" rel="noopener noreferrer" title="Vist the good folks at Electric Fun">@ElectricFun.</a></p>
   </div>
 </section>
