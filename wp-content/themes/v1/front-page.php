96c82634cefb40f342a94472cfccdf85191297d6
mattmaneval <manevalmusic@gmail.com>
2020-12-16 22:59:17 -0500

    Include form

--- wp-content/themes/v1/front-page.php
+++ wp-content/themes/v1/front-page.php
@@ -4,5 +4,6 @@
   include(locate_template("template-parts/hero.php"));
   include(locate_template("template-parts/about.php"));
   include(locate_template("template-parts/projects/projects-callout.php"));
+  include(locate_template("template-parts/contact.php"));
   get_footer();
 ?>
---
+++ wp-content/themes/v1/template-parts/contact.php
@@ -0,0 +1,10 @@
+<?php gravity_form_enqueue_scripts( 1, true ); ?>
+
+<section class="contact">
+  <div class="wrap">
+    <h3>Contact</h3>
+    <h2>Have an idea? <br />Let's have a conversation.</h2>
+    <p>I'm currently looking for new projects and new people to work with!</p>
+    <?php gravity_form( 1, false, false, false, '', true, 12 ); ?>
+  </div>
+</section>
