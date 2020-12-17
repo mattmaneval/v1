72e71590b51456d86f6d4964e980e29518096fd2
mattmaneval <manevalmusic@gmail.com>
2020-12-17 00:24:35 -0500

    type submit native override

--- wp-content/themes/v1/template-parts/contact.php
+++ wp-content/themes/v1/template-parts/contact.php
@@ -1,10 +1,12 @@
 <?php gravity_form_enqueue_scripts( 1, true ); ?>
 
-<section class="contact inner-wrap">
-  <div class="contact-form-card">
-    <h3>Contact</h3>
-    <h2>Have an idea? <br />Let's have a conversation.</h2>
-    <p>I'm currently looking for new projects and new people to work with!</p>
-    <?php gravity_form( 1, false, false, false, '', true, 12 ); ?>
+<section class="contact">
+  <div class="inner-wrap">
+    <div class="contact-form-card">
+      <h3>Contact</h3>
+      <h2>Have an idea? <br />Let's have a conversation.</h2>
+      <p>I'm currently looking for new projects and new people to work with!</p>
+      <?php gravity_form( 1, false, false, false, '', true, 12 ); ?>
+    </div>
   </div>
 </section>
