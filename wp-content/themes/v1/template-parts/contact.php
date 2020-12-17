2e6c0265c89bf95a6625b3a5ecf74db9c067db5d
mattmaneval <manevalmusic@gmail.com>
2020-12-17 00:04:28 -0500

    Basic form style

--- wp-content/themes/v1/template-parts/contact.php
+++ wp-content/themes/v1/template-parts/contact.php
@@ -1,7 +1,7 @@
 <?php gravity_form_enqueue_scripts( 1, true ); ?>
 
-<section class="contact">
-  <div class="wrap">
+<section class="contact inner-wrap">
+  <div class="contact-form-card">
     <h3>Contact</h3>
     <h2>Have an idea? <br />Let's have a conversation.</h2>
     <p>I'm currently looking for new projects and new people to work with!</p>
