407d4b2e163586497a3ef62c3977fb1cbeb9bd41
mattmaneval <manevalmusic@gmail.com>
2020-12-18 18:36:23 -0500

    Add wordmark / updates

--- wp-content/themes/v1/template-parts/contact.php
+++ wp-content/themes/v1/template-parts/contact.php
@@ -4,7 +4,6 @@
   <div class="contact-form-card">
     <h4>Have an idea?</h4>
     <h2>Let's have a conversation.</h2>
-    <p>I'm currently looking for new projects and new people to work with!</p>
     <?php gravity_form( 1, false, false, false, '', true, 12 ); ?>
   </div>
 </section>
