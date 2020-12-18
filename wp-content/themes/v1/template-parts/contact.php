3c8101fd8db52ad20fbd5f22490331e2d40fbd51
mattmaneval <manevalmusic@gmail.com>
2020-12-17 22:39:07 -0500

    install gravity forms wip

---
+++ wp-content/themes/v1/template-parts/contact.php
@@ -0,0 +1,10 @@
+<?php gravity_form_enqueue_scripts( 1, true ); ?>
+
+<section class="contact inner-wrap">
+  <div class="contact-form-card">
+    <h4>Have an idea?</h4>
+    <h1>Let's have a conversation.</h1>
+    <p>I'm currently looking for new projects and new people to work with!</p>
+    <?php gravity_form( 1, false, false, false, '', true, 12 ); ?>
+  </div>
+</section>
