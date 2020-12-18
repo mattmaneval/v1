0662fc68b901c1b75af418be787877dde4986086
mattmaneval <manevalmusic@gmail.com>
2020-12-18 00:17:19 -0500

    table links display

--- wp-content/themes/v1/template-parts/contact.php
+++ wp-content/themes/v1/template-parts/contact.php
@@ -1,9 +1,9 @@
 <?php gravity_form_enqueue_scripts( 1, true ); ?>
 
-<section class="contact inner-wrap">
+<section class="contact">
   <div class="contact-form-card">
     <h4>Have an idea?</h4>
-    <h1>Let's have a conversation.</h1>
+    <h2>Let's have a conversation.</h2>
     <p>I'm currently looking for new projects and new people to work with!</p>
     <?php gravity_form( 1, false, false, false, '', true, 12 ); ?>
   </div>
--- wp-content/themes/v1/template-parts/projects/projects.php
+++ wp-content/themes/v1/template-parts/projects/projects.php
@@ -11,14 +11,16 @@
     </td>
     <td class="tech hide-on-mobile"><?php echo $tech; ?></td>
     <td class="links">
-      <?php if(empty($repository_link)) {} else { ?>
-        <a href="<?php echo $repository_link; ?>" target="_blank" rel="noopener noreferrer" title="Visit the <?php the_title(); ?> repository on GitHub.">
-          <svg><use xlink:href="#code"/></svg>
+      <div>
+        <?php if(empty($repository_link)) {} else { ?>
+          <a href="<?php echo $repository_link; ?>" target="_blank" rel="noopener noreferrer" title="Visit the <?php the_title(); ?> repository on GitHub.">
+            <svg><use xlink:href="#code"/></svg>
+          </a>
+        <?php } ?>
+        <a href="<?php echo $project_link; ?>" target="_blank" rel="noopener noreferrer" title="Visit <?php the_title(); ?>">
+          <svg><use xlink:href="#external-link"/></svg>
         </a>
-      <?php } ?>
-      <a href="<?php echo $project_link; ?>" target="_blank" rel="noopener noreferrer" title="Visit <?php the_title(); ?>">
-        <svg><use xlink:href="#external-link"/></svg>
-      </a>
+      </div>
     </td>
   </tr>
 </tbody>
