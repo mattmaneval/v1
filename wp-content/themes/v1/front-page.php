3a5f0ee1603bd753d074431513ee39cc9ab8facf
mattmaneval <manevalmusic@gmail.com>
2020-12-17 21:36:44 -0500

    style update wip

--- wp-content/themes/v1/front-page.php
+++ wp-content/themes/v1/front-page.php
@@ -1,6 +1,8 @@
 <?php
   //Front Page
   get_header();
+  include(locate_template("template-parts/navigation/socials-bar.php"));
+  include(locate_template("template-parts/navigation/email-bar.php"));
   include(locate_template("template-parts/hero.php"));
   include(locate_template("template-parts/about.php"));
   include(locate_template("template-parts/projects/projects-callout.php"));
