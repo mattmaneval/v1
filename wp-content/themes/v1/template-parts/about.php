9c032e8aea3b52fdfacf9aecb856d58c5d54047a
mattmaneval <manevalmusic@gmail.com>
2020-12-19 00:19:42 -0500

    refactor

--- wp-content/themes/v1/template-parts/about.php
+++ wp-content/themes/v1/template-parts/about.php
@@ -3,20 +3,23 @@
     <h3>About</h3>
     <div class="about-card">
       <div class="content">
-        <h6>A brief history</h6>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <p>Outside of work I enjoy writing and recording music, running, and playing golf.</p>
-        <h6>Technologies I've been using:</h6>
-        <ul>
-          <li>Html</li>
-          <li>Css</li>
-          <li></li>
-          <li>Sass</li>
-          <li>React</li>
-          <li>Wordpress</li>
-          <li>Gatsby</li>
-        </ul>
+        <div class="about-bio">
+          <h6>A brief history</h6>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>Outside of work I enjoy writing and recording music, running, and playing golf.</p>
+        </div>
+        <div class="about-list">
+          <h6>Technologies I've been using:</h6>
+          <ul>
+            <li>Html</li>
+            <li>Css</li>
+            <li>Sass</li>
+            <li>React</li>
+            <li>Wordpress</li>
+            <li>Gatsby</li>
+          </ul>
+        </div>
       </div>
       <div class="about-potrait"></div>
     </div>
