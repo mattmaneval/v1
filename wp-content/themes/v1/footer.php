9d5948ce2539a2667662599c07abeaf8743687d9
mattmaneval <manevalmusic@gmail.com>
2020-12-18 20:16:21 -0500

    footer wip

--- wp-content/themes/v1/footer.php
+++ wp-content/themes/v1/footer.php
@@ -1,6 +1,6 @@
   	<footer>
       <div class="mini-wrap">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/v1-wordmark-black.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/v1-wordmark-black.png" alt="Matt Maneval wordmark">
         <p>This website runs on Wordpress and is hosted by WP Engine. It was designed and built by me (tools: Atom, MAMP, Gulp, GitHub). <p>
         <p>The type is set in "Joyride" by Type Juice, "Fanwood" by Barry Schwartz and "League Spartan" by Caroline Hadilaksono, Micah Rich & Tyler Finck ( Source: The League of Moveable Type).</p>
         <p>The desigin was inspired by <a href="https://brittanychiang.com/">Brittany Chiang</a>'s absolutely killer website.</p>
