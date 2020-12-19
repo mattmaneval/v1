79af8de5f72d2bee076dd162c293b5fec5e151a0
mattmaneval <manevalmusic@gmail.com>
2020-12-18 19:43:38 -0500

    refactor cta mixin

--- wp-content/themes/v1/template-parts/hero.php
+++ wp-content/themes/v1/template-parts/hero.php
@@ -1,5 +1,5 @@
 <section class="hero">
   <div class="wrap">
-    <h1>freelance web developer <span>based in toledo, ohio</span></h1>
+    <h1>freelance web developer <span>based in Toledo, Ohio</span></h1>
   </div>
 </section>
