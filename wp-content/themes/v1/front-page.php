32b3f75b0fb9ccfb8d323edfd013e84164489bb0
mattmaneval <manevalmusic@gmail.com>
2020-12-22 19:45:30 -0500

    Comment out unused social links

--- wp-content/themes/v1/front-page.php
+++ wp-content/themes/v1/front-page.php
@@ -4,7 +4,7 @@
   include(locate_template("template-parts/navigation/email-bar.php"));
   include(locate_template("template-parts/hero.php"));
   include(locate_template("template-parts/projects/projects-callout.php"));
-  include(locate_template("template-parts/about.php"));
+  // include(locate_template("template-parts/about.php"));
   include(locate_template("template-parts/contact.php"));
   get_footer();
 ?>
