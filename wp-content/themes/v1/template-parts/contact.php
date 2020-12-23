b74d5294a85fcaad640cad02161c8f5f34628307
mattmaneval <manevalmusic@gmail.com>
2020-12-23 16:16:38 -0500

    gform confirmation message

--- wp-content/themes/v1/template-parts/contact.php
+++ wp-content/themes/v1/template-parts/contact.php
@@ -4,6 +4,6 @@
   <div class="contact-form-card">
     <h4>Have an idea?</h4>
     <h2>Let's have a conversation.</h2>
-    <?php gravity_form( 1, false, false, false, null, true, 0 ); ?>
+    <?php gravity_form( 1, false, false, false, null, false, 0 ); ?>
   </div>
 </section>
