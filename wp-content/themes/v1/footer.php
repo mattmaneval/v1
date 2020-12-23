a8b589ddd8c8c19eddbd861d7c33b7e6bd401f36
mattmaneval <manevalmusic@gmail.com>
2020-12-22 19:41:47 -0500

    Footer touchups

--- wp-content/themes/v1/footer.php
+++ wp-content/themes/v1/footer.php
@@ -1,10 +1,12 @@
   	<footer>
-      <div class="mini-wrap">
+      <div class="wrap">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/v1-wordmark.png" alt="Matt Maneval wordmark">
-        <p>This website runs on Wordpress and is hosted by WP Engine. It was designed and built by me (tools: Atom, MAMP, Gulp, GitHub). <p>
-        <p>The type is set in "Joyride" by Type Juice, "Fanwood" by Barry Schwartz and "League Spartan" by Caroline Hadilaksono, Micah Rich & Tyler Finck ( Source: The League of Moveable Type).</p>
-        <p>The icons are from the <a href="https://feathericons.com/">Feather Icons</a> collection, created by Cole Bemis.</p>
-        <p>Elements of this site were inspired by <a href="https://brittanychiang.com/">Brittany Chiang</a>'s absolutely killer webstite.</p>
+        <div class="footer-flex">
+          <p>This website runs on Wordpress and is hosted by WP Engine. It was designed and built by me (tools: Atom, MAMP, Gulp, GitHub). </p>
+          <p>The type is set in "Joyride" by Type Juice, "Fanwood" by Barry Schwartz and "League Spartan" by Caroline Hadilaksono, Micah Rich & Tyler Finck ( Source: The League of Moveable Type).</p>
+          <p>The icons are from the <a href="https://feathericons.com/">Feather Icons</a> collection, created by Cole Bemis.</p>
+          <p>Elements of this site were inspired by <a href="https://brittanychiang.com/">Brittany Chiang</a>'s absolutely killer webstite.</p>
+        </div>
       </div>
   	</footer>
   <?php wp_footer(); ?>
