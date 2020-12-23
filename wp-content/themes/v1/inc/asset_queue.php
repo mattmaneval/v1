bb3865eaf5868ebaa7c30a3c0a8a9e5775ad7baa
mattmaneval <manevalmusic@gmail.com>
2020-12-23 18:06:03 -0500

    form fix wip

--- wp-content/themes/v1/inc/asset_queue.php
+++ wp-content/themes/v1/inc/asset_queue.php
@@ -5,6 +5,10 @@
     // Theme Stylesheet
     wp_enqueue_style( 'styles', get_stylesheet_uri(), "", '1.0.0', 'all' );
 
+    // jQuery
+      wp_deregister_script('jquery');
+      wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, true);
+
     // Theme Scripts
     wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/assets/js/build.js', "", '1.0.0', true );
   }
--- wp-content/themes/v1/template-parts/header.php
+++ wp-content/themes/v1/template-parts/header.php
@@ -4,7 +4,11 @@
       <img src="<?php echo get_template_directory_uri() ?>/assets/images/v1-wordmark.png" alt="">
     </div>
     <a class="cta" href="#contact" title="Get in touch">
-      <?php include(locate_template("template-parts/navigation/arrow-cta.php")); ?>
+      <?php
+        $arrow_cta_text ="get in touch";
+        include(locate_template("template-parts/navigation/arrow-cta.php"));
+        unset($arrow_cta_text);
+      ?>
     </a>
   </div>
 </div>
