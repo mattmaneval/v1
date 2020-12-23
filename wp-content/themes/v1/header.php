0cb97eecf688a5a238f328e9d54b951727608a5b
mattmaneval <manevalmusic@gmail.com>
2020-12-22 19:07:59 -0500

    meta tags

--- wp-content/themes/v1/header.php
+++ wp-content/themes/v1/header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<?php include( get_template_directory() . '/template-parts/head-meta.php'); ?>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
---
+++ wp-content/themes/v1/template-parts/head-meta.php
@@ -0,0 +1,18 @@
+<!-- Primary Meta Tags -->
+<title>Freelance Web Developer based in Toledo, Ohio.</title>
+<meta name="title" content="Freelance Web Developer based in Toledo, Ohio.">
+<meta name="description" content="Hi there, my name is Matt. I'm a freelance web developer in Toledo, Ohio. I specialize in creating pixel-perfect and performant websites.">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://metatags.io/">
+<meta property="og:title" content="Freelance Web Developer based in Toledo, Ohio.">
+<meta property="og:description" content="Hi there, my name is Matt. I'm a freelance web developer in Toledo, Ohio. I specialize in creating pixel-perfect and performant websites.">
+<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:title" content="Freelance Web Developer based in Toledo, Ohio.">
+<meta property="twitter:description" content="Hi there, my name is Matt. I'm a freelance web developer in Toledo, Ohio. I specialize in creating pixel-perfect and performant websites.">
+<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
