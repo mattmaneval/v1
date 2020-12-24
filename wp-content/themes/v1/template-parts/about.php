e24ea6d472434a396313641583dfa009130dd4d8
mattmaneval <manevalmusic@gmail.com>
2020-12-24 18:30:31 -0500

    Style about grid

--- wp-content/themes/v1/template-parts/about.php
+++ wp-content/themes/v1/template-parts/about.php
@@ -16,28 +16,3 @@
     </div>
   </div>
 </section>
-
-
-<!-- <h3>About</h3>
-<h6>I believe in excellence.</h6>
-<div class="about-card">
-  <div class="content">
-    <div class="about-bio">
-      <p>Hi there! My name is Matt and specialize in building exceptional websites. </p>
-      <p>It's my mission to create pixel-perfect and peformant products for my clients. Most recently, I’ve been fortunate to work with the good folks at Electric Fun, a boutique creative agency in Toledo, Ohio. I'm currently available for hire. </p>
-      <p>Outside of work I enjoy learning about new technology, writing and recording music, running, and playing golf.</p>
-    </div>
-    <div class="about-list">
-      <h6>Technologies I've been using:</h6>
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>sass</li>
-        <li>react</li>
-        <li>wordpress</li>
-        <li>gatsby</li>
-      </ul>
-    </div>
-  </div>
-  <div class="about-potrait"></div>
-</div> -->
