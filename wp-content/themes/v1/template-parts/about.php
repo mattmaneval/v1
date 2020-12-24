f8c2465ad9677b1536b221888b88eb8538507c80
mattmaneval <manevalmusic@gmail.com>
2020-12-24 13:32:14 -0500

    Add grid bas structure and style

--- wp-content/themes/v1/template-parts/about.php
+++ wp-content/themes/v1/template-parts/about.php
@@ -1,27 +1,37 @@
 <section class="about">
-  <div class="wrap">
-    <h3>About</h3>
-    <h6>I believe in excellence.</h6>
-    <div class="about-card">
-      <div class="content">
+  <div class="about-grid">
+
+      <div class="about-content">
+        <h2>I build things for the web.</h2>
         <div class="about-bio">
           <p>Hi there! My name is Matt and specialize in building exceptional websites. </p>
-          <p>It's my mission to create pixel-perfect and peformant products for my clients. Most recently, I’ve been fortunate to work with the good folks at Electric Fun, a boutique creative agency in Toledo, Ohio. I'm currently available for hire. </p>
-          <p>Outside of work I enjoy learning about new technology, writing and recording music, running, and playing golf.</p>
-        </div>
-        <div class="about-list">
-          <h6>Technologies I've been using:</h6>
-          <ul>
-            <li>html</li>
-            <li>css</li>
-            <li>sass</li>
-            <li>react</li>
-            <li>wordpress</li>
-            <li>gatsby</li>
-          </ul>
         </div>
       </div>
       <div class="about-potrait"></div>
+
+</section>
+
+
+<!-- <h3>About</h3>
+<h6>I believe in excellence.</h6>
+<div class="about-card">
+  <div class="content">
+    <div class="about-bio">
+      <p>Hi there! My name is Matt and specialize in building exceptional websites. </p>
+      <p>It's my mission to create pixel-perfect and peformant products for my clients. Most recently, I’ve been fortunate to work with the good folks at Electric Fun, a boutique creative agency in Toledo, Ohio. I'm currently available for hire. </p>
+      <p>Outside of work I enjoy learning about new technology, writing and recording music, running, and playing golf.</p>
+    </div>
+    <div class="about-list">
+      <h6>Technologies I've been using:</h6>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>sass</li>
+        <li>react</li>
+        <li>wordpress</li>
+        <li>gatsby</li>
+      </ul>
     </div>
   </div>
-</section>
+  <div class="about-potrait"></div>
+</div> -->
