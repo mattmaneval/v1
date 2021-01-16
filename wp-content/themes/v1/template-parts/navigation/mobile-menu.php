a3cb4c15c38072534f6424756988a638bc8be734
mattmaneval <manevalmusic@gmail.com>
2021-01-16 13:51:53 -0500

    wip

--- wp-content/themes/v1/template-parts/navigation/mobile-menu.php
+++ wp-content/themes/v1/template-parts/navigation/mobile-menu.php
@@ -1,8 +1,8 @@
 <div class="menu">
   <div class="wrap">
-    <a class="menu-link" href="#contact">About</a>
-    <a class="menu-link" href="#">Work</a>
-    <a class="menu-link" href="#">Contact</a>
+    <a class="menu-link" href="#about">About</a>
+    <a class="menu-link" href="#projects">Work</a>
+    <a class="menu-link" href="#contact">Contact</a>
     <?php include(locate_template("template-parts/resume/resume.php")); ?>
     <div class="mobile-socials-bar">
       <a class="socials-bar-link" target="_blank" href="https://github.com/mattmaneval" title="Github">
