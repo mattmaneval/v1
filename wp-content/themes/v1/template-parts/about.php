31bea00617ff9901016fb56174f8989fc21b77da
mattmaneval <manevalmusic@gmail.com>
2021-01-16 23:43:25 -0500

    hero and about section facelift

--- wp-content/themes/v1/template-parts/about.php
+++ wp-content/themes/v1/template-parts/about.php
@@ -1,14 +1,20 @@
 <section class="about" id="about">
-  <div class="mini-wrap">
+  <div class="wrap">
     <h4>Hello there!</h4>
     <h6>My name is Matt Maneval and I specialize in building exceptional websites.</h6>
-    <ul>
-      <li>
-        <svg><use xlink:href="#chevron-right" /></svg>
+    <div class="about-bio">
+      <div>
+        <h4>My mission</h4>
         <p>My goal is to always write modern, performant, maintainable code for websites and applications that matches the design pixel by pixel.</p>
+      </div>
+      <div>
+        <h4>What I'm up to</h4>
         <p>Most recently, I’ve built a custom PR site–Little Star PR–and an e-commerce platform–Ilmor Store–with the good folks at Electric Fun, a boutique creative agency in Toledo, Ohio.</p>
+      </div>
+      <div>
+        <h4>Interests</h4>
         <p>Outside of work I enjoy learning about new technology, dabbling in design, writing and recording music, and playing golf.</p>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </section>
